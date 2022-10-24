996f21ab3c50181a216c74af076869c8abc32a23
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-25 00:37:56 +0300

    поиск элемента

--- Tasks_functions/Program.cs
+++ Tasks_functions/Program.cs
@@ -1,12 +1,15 @@
 ﻿//I. Реализовать следующие функции для работы с массивами:
 
 //Печать массива на экран
+
 Console.WriteLine("Введите длину массива: ");
 int lengh = int.Parse(Console.ReadLine());
 double[] array = new double[lengh];
 Random random = new Random();
 for (int i = 0; i < lengh; i++)
     array[i] = (double)random.Next() % (lengh * 2) - lengh;
+
+
 // Поиск минимум
 double Min(double[] array)
 {
@@ -34,8 +37,6 @@ double Sum(double[] array)
         res += num;
     return res;
 }
-// Поиск произведения элементов массива
-
 double Prod(double[] array)
 {
     double res = 0.0;
@@ -43,7 +44,19 @@ double Prod(double[] array)
         res *= num;
     return res;
 }
+//Поиск индекса заданного элемента в массиве, если такого элемента в массиве нет то возвращать -1
 
+int Find(double[] array, double number)
+{
+    int res = -1;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] == number)
+        {
+            res = i;
+            return res;
+        }
+    return res;
+}
 
 //III. Вызвать и проверить работу всех вышеуказанных фукнций:
 
@@ -54,3 +67,7 @@ Console.WriteLine($"Минимум: {Min(array)}");
 Console.WriteLine($"Максимум: {Max(array)}");
 Console.WriteLine($"Поиск суммы элементов массива: {Sum(array)}");
 Console.WriteLine($"Поиск произведения элементов массива: {Prod(array)}");
+Console.WriteLine("Введите число для поиска в массиве: ");
+double number = double.Parse(Console.ReadLine()?? "0");
+Console.WriteLine($"Поиск индекса заданного элемента в массиве: {Find(array, number)}");
+
